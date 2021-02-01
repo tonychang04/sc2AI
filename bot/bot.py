3919a7780f24a28f7e124770c41f081994f0ea90
tony430 <tonychang430@hotmail.com>
2021-02-01 20:25:34 +0800

    added build worker mechanism for nexus

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,4 @@
-from sc2 import BotAI, Race
+from sc2 import BotAI, Race, UnitTypeId
 
 
 class CompetitiveBot(BotAI):
@@ -18,8 +18,19 @@ class CompetitiveBot(BotAI):
         # Do things here before the game starts
 
     async def on_step(self, iteration):
+        await self.distribute_workers()
+        await self.build_worker()
         # Populate this function with whatever your bot should do!
+
         pass
+    async def build_worker(self):
+        nexus = self.townhalls.ready.random
+        if (
+            self.can_afford(UnitTypeId.PROBE)
+            and nexus.is_idle
+            and self.workers.amount < self.townhalls.amount * 22
+        ):
+            nexus.train(UnitTypeId.PROBE)
 
     def on_end(self, result):
         print("Game ended.")
